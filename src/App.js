929d88c877d019e77fe4fcc46c03881c3ca1c4c0
aescastro <adriana.e.castro@outlook.com>
2021-08-20 12:52:35 -0600

    added ability to have an timeline element which does not have an image

--- src/App.js
+++ src/App.js
@@ -27,7 +27,6 @@ class TimelineElement extends React.Component {
       title: props.title,
       img: props.img,
       desc: props.desc,
-      visible: true,
     }
 
   }
@@ -42,10 +41,14 @@ class TimelineElement extends React.Component {
       
         <h3 className="vertical-timeline-element-title" style={{paddingBottom: "10px"}}>{this.state.title}</h3>
          
-        <div className="image-and-caption">
+        <div className="image-and-caption"
+                      style={{display: this.state.img === undefined ? 'none' : 'block',
+                              float: this.state.img === undefined ? 'none' : 'left'}}
+                      >
           <img  src={this.state.img}
                 id="event-image"
-                alt="">
+                alt=""
+                >
           </img>
         </div>
 
@@ -54,7 +57,6 @@ class TimelineElement extends React.Component {
             {this.state.desc}
           </p>
         </div>
-        
         
       </VerticalTimelineElement>
     );
@@ -70,7 +72,11 @@ class App extends React.Component {
             title="test reuse" 
             img="https://image.shutterstock.com/image-vector/smile-icon-vector-face-emoticon-260nw-1721368459.jpg" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident">
           </TimelineElement>
-          
+          <TimelineElement 
+            title="test reuse" 
+            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident">
+          </TimelineElement>
+
           <DummyElement></DummyElement>
           <DummyElement></DummyElement>
           <DummyElement></DummyElement>

2b2fa5c7682af4dea50cf1ba932707a8abdde6a7
aescastro <adriana.e.castro@outlook.com>
2021-08-20 15:26:34 -0600

    added date field plus more generic css classes

--- src/App.js
+++ src/App.js
@@ -25,9 +25,12 @@ class TimelineElement extends React.Component {
 
     this.state = {
       title: props.title,
+      date: props.date,
       img: props.img,
       desc: props.desc,
     }
+
+    console.log(this.state.date);
 
   }
 
@@ -39,7 +42,7 @@ class TimelineElement extends React.Component {
         iconStyle={{ background: '#586BA4', color: '#fff' }}
       >
       
-        <h3 className="vertical-timeline-element-title" style={{paddingBottom: "10px"}}>{this.state.title}</h3>
+        <h3 className="vertical-timeline-element-title" style={{marginBottom: "10px"}}>{this.state.title}</h3>
          
         <div className="image-and-caption"
                       style={{display: this.state.img === undefined ? 'none' : 'block',
@@ -52,8 +55,10 @@ class TimelineElement extends React.Component {
           </img>
         </div>
 
-        <div className="event-text">
-          <p>
+        <div className="event-info">
+          <div className="desc-header"> Date: <span className="event-text">{this.state.date}</span> </div>
+          <div className="desc-header">Description: </div>
+          <p className="event-text">
             {this.state.desc}
           </p>
         </div>
@@ -70,6 +75,7 @@ class App extends React.Component {
         <VerticalTimeline>
           <TimelineElement 
             title="test reuse" 
+            date="08/01/2021"
             img="https://image.shutterstock.com/image-vector/smile-icon-vector-face-emoticon-260nw-1721368459.jpg" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident">
           </TimelineElement>
           <TimelineElement 

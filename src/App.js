3137f073a7e7ce06821b8ff73e4fb471a7b01cee
aescastro <adriana.e.castro@outlook.com>
2021-10-20 19:43:09 -0700

    color change again

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'; 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Badge from 'react-bootstrap/Badge'
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
@@ -9,7 +10,7 @@ function DummyElement() {
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
         contentArrowStyle={{ borderRight: '7px solid  #fff' }}
-        iconStyle={{ background: '#003B36', color: '#fff' }}
+        iconStyle={{ background: '#553E4E', color: '#fff' }}
         contentStyle={{background: "#fff", }}
       >
         <h3 className="vertical-timeline-element-title">Lorem ipsum dolor</h3>
@@ -39,8 +40,7 @@ function DescriptionElement(props) {
 function TaggedElements(props) {
   if (props.info) {
     var tags = new Set(props.info);
-    tags = Array.from(tags).map(x =>
-      <span className="tag" key={x} style={{backgroundColor: props.color, borderColor: props.color}}>{x}</span>)
+    tags = Array.from(tags).map(x => <Badge bg="primary" style={{margin: "1px"}}>{x}</Badge>)
       
       return (<div className="desc-container"> 
                 {props.title} {tags}
@@ -68,7 +68,7 @@ class TimelineElement extends React.Component {
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
         contentArrowStyle={{ borderRight: '7px solid  #fff' }}
-        iconStyle={{ background: '#003B36', color: '#fff' }}
+        iconStyle={{ background: '#553E4E', color: '#fff' }}
         contentStyle={{background: "#fff"}}
       >
       
@@ -86,10 +86,10 @@ class TimelineElement extends React.Component {
         </div>
 
         <div className="event-info">
-          <DescriptionElement title="Date:" info={this.state.date}></DescriptionElement>
-          <TaggedElements title="People:" info={this.state.people} color="59114D"></TaggedElements>
-          <TaggedElements title="Tags:" info={this.state.tags} color="#59114D"></TaggedElements>
-          <DescriptionElement title="Decription:" info={this.state.desc}></DescriptionElement>
+          <DescriptionElement title="Date: " info={this.state.date}></DescriptionElement>
+          <TaggedElements title="People: " info={this.state.people} color=""></TaggedElements>
+          <TaggedElements title="Tags: " info={this.state.tags} color="#D3AB9E"></TaggedElements>
+          <DescriptionElement title="Decription: " info={this.state.desc}></DescriptionElement>
         </div>
         
       </VerticalTimelineElement>
@@ -103,24 +103,26 @@ class App extends React.Component {
     var tags = ["food", "vacation", "food"];
 
     return (
-        <VerticalTimeline>
-          <TimelineElement 
-            title="test reuse" 
-            date="08/01/2021"
-            tags={tags}
-            img="https://image.shutterstock.com/image-vector/smile-icon-vector-face-emoticon-260nw-1721368459.jpg" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident">
-          </TimelineElement>
-          <TimelineElement 
-            title="test reuse" 
-            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident">
-          </TimelineElement>
 
-          <DummyElement></DummyElement>
-          <DummyElement></DummyElement>
-          <DummyElement></DummyElement>
-          <DummyElement></DummyElement>
-          
-        </VerticalTimeline>
+      <VerticalTimeline>
+        <TimelineElement 
+          title="test reuse" 
+          date="08/01/2021"
+          tags={tags}
+          img="https://image.shutterstock.com/image-vector/smile-icon-vector-face-emoticon-260nw-1721368459.jpg" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident">
+        </TimelineElement>
+        <TimelineElement 
+          title="test reuse" 
+          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident">
+        </TimelineElement>
+
+        <DummyElement></DummyElement>
+        <DummyElement></DummyElement>
+        <DummyElement></DummyElement>
+        <DummyElement></DummyElement>
+        
+      </VerticalTimeline>
+        
     );
  }
 }
